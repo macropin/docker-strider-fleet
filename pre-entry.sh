048ed91151b57873578d9d40cec1517220946a72
Andrew Cutler <macropin@gmail.com>
2015-07-29 14:00:54 +1000

    add status messages

--- pre-entry.sh
+++ pre-entry.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+echo "$(basename $0) >> Running"
+
 # Make .strider ephemeral. Link to /tmp to use local host io.
 if [ ! -L "/data/.strider" ]; then
+    echo "$(basename $0) >> Adding symlink for /tmp/strider"
     rm -rf /data/.strider
     ln -s /data/.strider /tmp/strider
 fi
